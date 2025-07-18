566f365c953f3a48e27765f982abe759e5f870a2
zhengjiajun <15824456449@163.com>
2025-07-18 17:23:23 +0800

    v1.1.0 全局配置加载 基本功能

---
+++ example-consumer/src/main/java/com/zheng/consumer/ConsumerExample.java
@@ -0,0 +1,15 @@
+package com.zheng.consumer;
+
+import com.zheng.conf.RpcConfig;
+import com.zheng.utils.ConfigUtils;
+
+/**
+ * 简易服务消费者示例
+ */
+public class ConsumerExample {
+
+    public static void main(String[] args) {
+        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
+        System.out.println(rpc);
+    }
+}
--- example-provider/src/main/java/com/zheng/provider/EasyProviderExample.java
+++ example-provider/src/main/java/com/zheng/provider/EasyProviderExample.java
@@ -1,5 +1,6 @@
 package com.zheng.provider;
 
+import com.zheng.RpcApplication;
 import com.zheng.registry.LocalRegistry;
 import com.zheng.server.HttpServer;
 import com.zheng.server.VertxHttpServer;
@@ -7,15 +8,19 @@ import com.zheng.service.UserService;
 
 /**
  * 简易服务提供者示例
+ *
  */
 public class EasyProviderExample {
 
     public static void main(String[] args) {
+        // RPC 框架初始化
+        RpcApplication.init();
+        
         // 注册服务
         LocalRegistry.register(UserService.class.getName(), UserServiceImpl.class);
 
         // 启动 web 服务
         HttpServer httpServer = new VertxHttpServer();
-        httpServer.doStart(8080);
+        httpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
     }
 }
