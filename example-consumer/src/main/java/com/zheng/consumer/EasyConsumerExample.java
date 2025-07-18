850da4beefbcfbc6b924a83c8641b05fa872ddd3
zhengjiajun <15824456449@163.com>
2025-07-18 16:56:53 +0800

    v1.0.0 简易版手写RPC

---
+++ example-consumer/src/main/java/com/zheng/consumer/EasyConsumerExample.java
@@ -0,0 +1,25 @@
+package com.zheng.consumer;
+
+import com.zheng.model.User;
+import com.zheng.proxy.ServiceProxyFactory;
+import com.zheng.service.UserService;
+
+/**
+ * 简易服务消费者示例
+ */
+public class EasyConsumerExample {
+
+    public static void main(String[] args) {
+        // todo 需要获取 UserService 的实现类对象
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("zhengjiajun");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
+    }
+}
---
+++ example-provider/src/main/java/com/zheng/provider/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package com.zheng.provider;
+
+import com.zheng.model.User;
+import com.zheng.service.UserService;
+
+/**
+ * 用户服务实现类
+ */
+public class UserServiceImpl implements UserService {
+
+    public User getUser(User user) {
+        System.out.println("用户名：" + user.getName());
+        return user;
+    }
+}
