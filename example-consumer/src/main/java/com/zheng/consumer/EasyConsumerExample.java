ad220c1e9c2f22954dc67d99c296ad7d31f7a02e
zhengjiajun <15824456449@163.com>
2025-07-18 23:33:15 +0800

    v1.2.0 接口Mock 完成Mock测试开发

--- example-consumer/src/main/java/com/zheng/consumer/EasyConsumerExample.java
+++ example-consumer/src/main/java/com/zheng/consumer/EasyConsumerExample.java
@@ -21,5 +21,8 @@ public class EasyConsumerExample {
         } else {
             System.out.println("user == null");
         }
+
+        long number = userService.getNumber();
+        System.out.println(number);
     }
 }
---
+++ zz-rpc-core/src/main/java/com/zheng/proxy/MockServiceProxy.java
@@ -0,0 +1,53 @@
+package com.zheng.proxy;
+
+import com.github.javafaker.Faker;
+import lombok.extern.slf4j.Slf4j;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Locale;
+
+@Slf4j
+public class MockServiceProxy implements InvocationHandler {
+
+    private final Faker faker = new Faker(Locale.CHINA);
+
+    /**
+     * 调用代理
+     * @return
+     * @throws Throwable
+     */
+    @Override
+    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+        Class<?> methodReturn = method.getReturnType();
+        log.info("mock invoke {}", method.getName());
+        return getDefaultObject(methodReturn);
+    }
+
+    /**
+     * 生成指定类型的默认值对象（可自行完善默认值逻辑）
+     * @param type
+     * @return
+     */
+    private Object getDefaultObject(Class<?> type) {
+        // isPrimitive()判断是否为基本类型
+        if(type.isPrimitive()){
+            if(type == boolean.class){
+                return false;
+            }else if(type == int.class){
+                return faker.number().randomDigit();
+            }else if(type == short.class){
+                return (short) 0;
+            }else if(type == long.class){
+                return 0L;
+            }else if(type == float.class){
+                return 0f;
+            }else if(type == double.class){
+                return 0D;
+            }else if(type == char.class){
+                return (char)0;
+            }
+        }
+        return null;
+    }
+}
