96c701947fe5ef11648b7a205316d060c60da0b4
zhengjiajun <15824456449@163.com>
2025-07-20 13:27:46 +0800

    v1.9.0 启动机制和注解驱动 重构服务提供者和服务消费者的初始化流程
    - 新增 ProviderBootstrap 和 ConsumerBootstrap 类，分别负责服务提供者和服务消费者的初始化
    - 重构 ProviderExample 和 ConsumerExample 类，使用新的初始化类
    - 新增 ServiceRegisterInfo 类，用于存储服务注册信息
    - 优化服务注册和启动逻辑，提高代码复用性和可维护性

--- example-consumer/src/main/java/com/zheng/consumer/ConsumerExample.java
+++ example-consumer/src/main/java/com/zheng/consumer/ConsumerExample.java
@@ -1,15 +1,26 @@
 package com.zheng.consumer;
 
-import com.zheng.conf.RpcConfig;
-import com.zheng.utils.ConfigUtils;
+import com.zheng.bootstrap.ConsumerBootstrap;
+import com.zheng.model.User;
+import com.zheng.proxy.ServiceProxyFactory;
+import com.zheng.service.UserService;
 
-/**
- * 简易服务消费者示例
- */
 public class ConsumerExample {
 
     public static void main(String[] args) {
-        RpcConfig rpc = ConfigUtils.loadConfig(RpcConfig.class, "rpc");
-        System.out.println(rpc);
+        // 服务提供者初始化
+        ConsumerBootstrap.init();
+
+        // 获取代理
+        UserService userService = ServiceProxyFactory.getProxy(UserService.class);
+        User user = new User();
+        user.setName("zheng");
+        // 调用
+        User newUser = userService.getUser(user);
+        if (newUser != null) {
+            System.out.println(newUser.getName());
+        } else {
+            System.out.println("user == null");
+        }
     }
 }
--- example-provider/src/main/java/com/zheng/provider/ProviderExample.java
+++ example-provider/src/main/java/com/zheng/provider/ProviderExample.java
@@ -1,47 +1,28 @@
 package com.zheng.provider;
 
-import cn.hutool.core.net.NetUtil;
-import com.zheng.RpcApplication;
-import com.zheng.conf.RpcConfig;
-import com.zheng.model.ServiceMetaInfo;
-import com.zheng.registry.LocalRegistry;
-import com.zheng.registry.Registry;
-import com.zheng.registry.RegistryConfig;
-import com.zheng.registry.RegistryFactory;
-import com.zheng.server.tcp.VertxTcpServer;
+
+
+import com.zheng.bootstrap.ProviderBootstrap;
+import com.zheng.model.ServiceRegisterInfo;
 import com.zheng.service.UserService;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * 服务提供者示例
+ *
+
  */
 public class ProviderExample {
 
     public static void main(String[] args) {
-        // RPC 框架初始化
-        RpcApplication.init();
+        // 要注册的服务
+        List<ServiceRegisterInfo<?>> serviceRegisterInfoList = new ArrayList<>();
+        ServiceRegisterInfo serviceRegisterInfo = new ServiceRegisterInfo(UserService.class.getName(), UserServiceImpl.class);
+        serviceRegisterInfoList.add(serviceRegisterInfo);
 
-        // 注册服务
-        String serviceName = UserService.class.getName();
-        LocalRegistry.register(serviceName, UserServiceImpl.class);
-
-        // 注册服务到注册中心
-        RpcConfig rpcConfig = RpcApplication.getRpcConfig();
-        RegistryConfig registryConfig = rpcConfig.getRegistryConfig();
-        Registry registry = RegistryFactory.getInstance(registryConfig.getRegistry());
-        ServiceMetaInfo serviceMetaInfo = new ServiceMetaInfo();
-        serviceMetaInfo.setServiceName(serviceName);
-        serviceMetaInfo.setServiceHost(rpcConfig.getServerHost());
-        serviceMetaInfo.setServicePort(rpcConfig.getServerPort());
-
-
-        try {
-            registry.register(serviceMetaInfo);
-        } catch (Exception e) {
-            throw new RuntimeException(e);
-        }
-
-        // 启动 TCP 服务
-        VertxTcpServer vertxTcpServer = new VertxTcpServer();
-        vertxTcpServer.doStart(RpcApplication.getRpcConfig().getServerPort());
+        // 服务提供者初始化
+        ProviderBootstrap.init(serviceRegisterInfoList);
     }
 }
