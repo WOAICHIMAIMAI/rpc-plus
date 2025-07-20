0c1fc6a372ca6c3cb96ff3fc95ea41f02ded559b
zhengjiajun <15824456449@163.com>
2025-07-20 13:46:25 +0800

    v.1.10.0 启动机制和注解驱动 添加 Spring Boot 自动配置功能
    - 新增 EnableRpc 注解，用于启动 RPC 服务
    - 添加 RpcConsumerBootstrap、RpcInitBootstrap 和 RpcProviderBootstrap 启动类
    - 新增 RpcReference 和 RpcService 注解，用于服务注入和注册- 更新项目配置，添加新模块支持

---
+++ zz-rpc-spring-boot-starter/src/main/java/com/zheng/zzrpcspringbootstarter/annotation/EnableRpc.java
@@ -0,0 +1,24 @@
+package com.zheng.zzrpcspringbootstarter.annotation;
+
+import com.zheng.zzrpcspringbootstarter.bootstrap.RpcConsumerBootstrap;
+import com.zheng.zzrpcspringbootstarter.bootstrap.RpcInitBootstrap;
+import com.zheng.zzrpcspringbootstarter.bootstrap.RpcProviderBootstrap;
+import org.springframework.context.annotation.Import;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Import({RpcInitBootstrap.class, RpcProviderBootstrap.class, RpcConsumerBootstrap.class})
+public @interface EnableRpc {
+
+    /**
+     * 需要启动 server
+     *
+     * @return
+     */
+    boolean needServer() default true;
+}
